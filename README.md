5e37607e7513f18b80c8620f99f5488ea0505cda
lizhanhui <lizhanhui@gmail.com>
2024-01-05 08:55:16 +0800

    Implement broadcast-multi-node challenge
    Signed-off-by: lizhanhui <lizhanhui@gmail.com>

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ https://fly.io/dist-sys/
 ```sh
 ~/Downloads/maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 1 --time-limit 10
 ```
+
+# Unique-Ids
+```sh
+~/Downloads/maelstrom/maelstrom test -w unique-ids --bin target/debug/unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+```
+
+# Broadcast
+
+## Single-Node Broadcast
+
+```sh
+~/Downloads/maelstrom/maelstrom test -w broadcast --bin target/debug/broadcast --node-count 1 --time-limit 20 --rate 10
+```
+
+## Multi-Node Broadcast
+
+```sh
+~/Downloads/maelstrom/maelstrom test -w broadcast --bin target/debug/broadcast --node-count 5 --time-limit 20 --rate 10
+```
--- src/node/broadcast.rs
+++ src/node/broadcast.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, io::Write};
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    message::{Init, Message},
+    message::{Body, Init, Message},
     Node,
 };
 
@@ -31,6 +31,8 @@ pub struct BroadcastNode {
     id: usize,
 
     vals: Vec<usize>,
+
+    neighbors: Vec<String>,
 }
 
 impl Node<BroadcastPayload> for BroadcastNode {
@@ -43,6 +45,7 @@ impl Node<BroadcastPayload> for BroadcastNode {
             node_ids: init.node_ids.clone(),
             id: 0,
             vals: Vec::new(),
+            neighbors: Vec::new(),
         })
     }
 
@@ -55,9 +58,34 @@ impl Node<BroadcastPayload> for BroadcastNode {
 
         match reply.body.payload {
             BroadcastPayload::Broadcast { message } => {
+                let mut new = false;
                 if self.vals.iter().find(|&v| *v == message).is_none() {
                     self.vals.push(message);
+                    new = true;
                 }
+
+                if new {
+                    for peer in &self.neighbors {
+                        // Skip the original node
+                        if reply.dst == *peer {
+                            continue;
+                        }
+                        let mid = self.id;
+                        self.id += 1;
+                        let gossip = Message::<BroadcastPayload> {
+                            src: self.node_id.clone(),
+                            dst: peer.clone(),
+                            body: Body::<BroadcastPayload> {
+                                id: Some(mid),
+                                in_reply_to: None,
+                                payload: BroadcastPayload::Broadcast { message: message },
+                            },
+                        };
+                        // broadcast new message to its neighbours.
+                        gossip.write_and_flush(&mut *write)?;
+                    }
+                }
+
                 reply.body.payload = BroadcastPayload::BroadcastOk;
             }
             BroadcastPayload::Read => {
@@ -65,7 +93,10 @@ impl Node<BroadcastPayload> for BroadcastNode {
                     messages: self.vals.clone(),
                 };
             }
-            BroadcastPayload::Topology { topology: _ } => {
+            BroadcastPayload::Topology { topology } => {
+                if let Some(n) = topology.get(&self.node_id) {
+                    self.neighbors.append(&mut n.clone());
+                }
                 reply.body.payload = BroadcastPayload::TopologyOk;
             }
 
